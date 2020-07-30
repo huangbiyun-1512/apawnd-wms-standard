5573e99e10062b4646a9655725269e543fd93aec
Nick <huangbiyun1512@163.com>
2020-07-30 17:51:12 +0800

    init

--- poc-api/src/main/java/com/example/poc/api/HealthCheckApi.java
+++ poc-api/src/main/java/com/example/poc/api/HealthCheckApi.java
@@ -1,9 +1,21 @@
 package com.example.poc.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 public interface HealthCheckApi {
 
+  @Operation(summary = "API Health Check")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK",
+          content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+      @ApiResponse(responseCode = "500", description = "Server Internal Error",
+          content = @Content)
+  })
   ResponseEntity isAlive();
 
 }
--- poc-provider/src/main/java/com/example/poc/controller/v1/HealthCheckApiController.java
+++ poc-provider/src/main/java/com/example/poc/controller/v1/HealthCheckApiController.java
@@ -2,6 +2,7 @@ package com.example.poc.controller.v1;
 
 import com.example.poc.api.HealthCheckApi;
 import com.example.poc.components.annotation.AutoLogging;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckApiController implements HealthCheckApi {
 
   @Override
-  @GetMapping("/health")
+  @GetMapping(value = "/health", produces = MediaType.APPLICATION_JSON_VALUE)
   @AutoLogging
   public ResponseEntity isAlive() {
     return ResponseEntity.ok(true);
