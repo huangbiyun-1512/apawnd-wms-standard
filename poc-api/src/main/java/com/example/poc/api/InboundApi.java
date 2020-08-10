9c493bb2d82c4f3d9c5a6157ea636d29ebca58e7
Nick <huangbiyun1512@163.com>
2020-08-10 15:16:14 +0800

    feature: new API

--- poc-api/src/main/java/com/example/poc/api/InboundApi.java
+++ poc-api/src/main/java/com/example/poc/api/InboundApi.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 
 public interface InboundApi {
 
-  @Operation(summary = "Create purchase order.", tags = {"inbound"})
+  @Operation(summary = "Create a new purchase order.", tags = {"inbound"})
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "OK",
           content = {@Content(
@@ -32,5 +32,22 @@ public interface InboundApi {
           description = "Purchase order info DTO.",
           required = true,
           schema = @Schema(implementation = PoDto.class)) PoDto poDto);
+
+  @Operation(summary = "Query an existed purchase order by po number and warehouse id.", tags = {"inbound"})
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "OK",
+          content = {@Content(
+              schema = @Schema(implementation = BaseResponseDto.class))}),
+      @ApiResponse(responseCode = "400", description = "Bad Request",
+          content = {@Content(
+              schema = @Schema(implementation = BaseResponseDto.class))}),
+      @ApiResponse(responseCode = "401", description = "Unauthorized",
+          content = {@Content(
+              schema = @Schema(implementation = BaseResponseDto.class))}),
+      @ApiResponse(responseCode = "500", description = "Server Internal Error",
+          content = @Content(
+              schema = @Schema(implementation = BaseResponseDto.class)))
+  })
+  ResponseEntity queryPurchaseOrder(String poNumber, String whId);
 
 }
--- poc-provider/src/main/java/com/example/poc/controller/v1/InboundController.java
+++ poc-provider/src/main/java/com/example/poc/controller/v1/InboundController.java
@@ -1,15 +1,13 @@
 package com.example.poc.controller.v1;
 
 import com.common.poc.components.annotation.AutoLogging;
+import com.common.poc.components.dto.BaseResponseDto;
 import com.example.poc.api.InboundApi;
 import com.example.poc.dto.PoDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -19,14 +17,28 @@ import javax.validation.Valid;
 public class InboundController implements InboundApi {
 
   @Override
-  @PostMapping(value = "/purchase_orders", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping(value = "/po", produces = MediaType.APPLICATION_JSON_VALUE)
   @AutoLogging
   public ResponseEntity createPurchaseOrder(@Valid @RequestBody PoDto poDto) {
-    log.info("po_number: {}", poDto.getPoNumber());
-    log.info("create_date: {}", poDto.getCreateDate());
-    log.info("order_pcs: {}", poDto.getOrderPcs());
-    log.info("lp_sequence: {}", poDto.getLpSequence());
-    log.info("generic_field1: {}", poDto.getGenericField1());
-    return ResponseEntity.created(null).build();
+//    log.info("po_number: {}", poDto.getPoNumber());
+//    log.info("create_date: {}", poDto.getCreateDate());
+//    log.info("order_pcs: {}", poDto.getOrderPcs());
+//    log.info("lp_sequence: {}", poDto.getLpSequence());
+//    log.info("generic_field1: {}", poDto.getGenericField1());
+
+    return ResponseEntity
+        .created(null)
+        .body(BaseResponseDto.ok());
+  }
+
+  @Override
+  @GetMapping(value = "/po", produces = MediaType.APPLICATION_JSON_VALUE)
+  @AutoLogging
+  public ResponseEntity queryPurchaseOrder(
+      @RequestParam("po_number") String poNumber,
+      @RequestParam("wh_id") String whId) {
+//    log.info("po_number: {}", poNumber);
+//    log.info("wh_id: {}", whId);
+    return ResponseEntity.ok(BaseResponseDto.fail());
   }
 }
