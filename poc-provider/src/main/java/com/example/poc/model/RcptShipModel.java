5892d633b737bc2dba2510a6f475864b3b980f8c
Nick <huangbiyun1512@163.com>
2020-08-19 15:26:07 +0800

    feature: ASN models

---
+++ poc-provider/src/main/java/com/example/poc/model/RcptShipModel.java
@@ -0,0 +1,33 @@
+package com.example.poc.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+public class RcptShipModel {
+
+  private String whId;
+	private String shipmentNumber;
+	private Integer carrierId;
+	private String trailerNumber;
+	private Instant dateExpected;
+	private Instant dateReceived;
+  private Instant dateShipped;
+  private String status;
+	private String comments;
+  private Integer workersAssigned;
+	private String proNumber;
+  private String arrivalSign;
+  private Instant arrivalDate;
+  private String grnSendSign;
+  private Instant grnSendDate;
+  private Instant shipmentReceiptDate;
+  private String asnType;
+  private String wipNumber;
+  private String fileSeq;
+  private String tallysheetIsPrinted;
+
+}
---
+++ poc-provider/src/main/java/com/example/poc/model/RcptShipPoModel.java
@@ -0,0 +1,20 @@
+package com.example.poc.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+public class RcptShipPoModel {
+
+  private String whId;
+	private String shipmentNumber;
+	private String poNumber;
+	private BigDecimal casesExpected;
+  private BigDecimal casesReceived;
+	private Instant openToBuyDate;
+
+}
