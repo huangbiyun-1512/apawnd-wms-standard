52c03b7e089b5fb268e22da43fcc21affd3b790c
Nick <huangbiyun1512@163.com>
2020-09-08 16:02:19 +0800

    feature: i18n

--- poc-provider/src/main/java/com/example/poc/service/impl/OutboundServiceImpl.java
+++ poc-provider/src/main/java/com/example/poc/service/impl/OutboundServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.poc.service.impl;
 
+import com.example.poc.component.util.MessageUtil;
 import com.example.poc.service.OutboundService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,8 +9,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class OutboundServiceImpl implements OutboundService {
 
+  private final MessageUtil messageUtil;
+
+  public OutboundServiceImpl(MessageUtil messageUtil) {
+    this.messageUtil = messageUtil;
+  }
+
   @Override
   public void scaLoadedAck() {
     log.info("SCA loaded ack executing...");
+
+    // Retrieve value of I01-0001.code
+    messageUtil.getCode("I01-0001");
+    // Retrieve value of I01-0001.title
+    messageUtil.getTitle("I01-0001");
+    // Retrieve value of I01-0001.detail
+    messageUtil.getDetail("I01-0001");
+
   }
 }
