2cdbada0a2f564d3f7ebf50abed55302a201cbb4
Nick <huangbiyun1512@163.com>
2020-09-09 10:28:05 +0800

    feature: redis integration

--- poc-provider/src/main/java/com/example/poc/config/JacksonConfig.java
+++ poc-provider/src/main/java/com/example/poc/config/JacksonConfig.java
@@ -1,6 +1,8 @@
 package com.example.poc.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
