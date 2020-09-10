763baa5c01af429d10b32b3a75b8dfc93f261559
Nick <huangbiyun1512@163.com>
2020-09-10 10:01:51 +0800

    feature: cache

--- poc-provider/src/main/java/com/example/poc/DomainApplication.java
+++ poc-provider/src/main/java/com/example/poc/DomainApplication.java
@@ -2,6 +2,7 @@ package com.example.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +15,7 @@ import java.util.TimeZone;
 @ComponentScan(basePackages = {"com.*"})
 @EnableDiscoveryClient
 @EnableScheduling
+@EnableCaching
 public class DomainApplication {
 
   public static void main(String[] args) {
