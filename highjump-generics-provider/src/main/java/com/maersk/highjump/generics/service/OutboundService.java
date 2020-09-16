068e2ae7b71e88ebd843d878de68842324a55eea
Nick <huangbiyun1512@163.com>
2020-09-16 15:03:07 +0800

    feature: package adjust

--- highjump-generics-provider/src/main/java/com/maersk/highjump/generics/service/OutboundService.java
+++ highjump-generics-provider/src/main/java/com/maersk/highjump/generics/service/OutboundService.java
@@ -3,10 +3,4 @@ package com.maersk.highjump.generics.service;
 public interface OutboundService {
 
   void scaLoadedAck();
-
-  Object retrieveDataFromCacheIfExisted(String key);
-
-  void updateCache(String key);
-
-  void removeCache(String key);
 }
--- highjump-generics-provider/src/main/java/com/maersk/highjump/generics/service/impl/OutboundServiceImpl.java
+++ highjump-generics-provider/src/main/java/com/maersk/highjump/generics/service/impl/OutboundServiceImpl.java
@@ -1,55 +1,16 @@
 package com.maersk.highjump.generics.service.impl;
 
-import com.maersk.commons.component.util.MessageUtil;
 import com.maersk.highjump.generics.service.OutboundService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class OutboundServiceImpl implements OutboundService {
 
-  private final MessageUtil messageUtil;
-
-  public OutboundServiceImpl(MessageUtil messageUtil) {
-    this.messageUtil = messageUtil;
-  }
-
   @Override
   public void scaLoadedAck() {
     log.info("SCA loaded ack executing...");
-//    log.warn("This is a waring log.");
-//    log.error("This is a error log.");
-//    log.debug("This is a debug log.");
-//    log.trace("This is a trace log.");
-//
-//    // Retrieve value of I01-0001.code
-//    messageUtil.getCode("I01-0001");
-//    // Retrieve value of I01-0001.title
-//    messageUtil.getTitle("I01-0001");
-//    // Retrieve value of I01-0001.detail
-//    messageUtil.getDetail("I01-0001");
-
   }
 
-  @Cacheable(key = "#key")
-  @Override
-  public Object retrieveDataFromCacheIfExisted(String key) {
-    return "cache value";
-  }
-
-  @CachePut(key = "#key")
-  @Override
-  public void updateCache(String key) {
-
-  }
-
-  @CacheEvict(key = "#key")
-  @Override
-  public void removeCache(String key) {
-
-  }
 }
