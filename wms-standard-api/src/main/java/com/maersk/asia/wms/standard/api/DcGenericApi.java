ab6ee12f20a38a2481df892bf5a5cea3fcd9698c
Nick <huangbiyun1512@163.com>
2020-09-29 15:15:57 +0800

    feature: optimize

--- wms-standard-api/src/main/java/com/maersk/asia/wms/standard/api/DcGenericApi.java
+++ wms-standard-api/src/main/java/com/maersk/asia/wms/standard/api/DcGenericApi.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 @FeignClient(name = "highjump-generics")
 public interface DcGenericApi {
 
-  @Operation(summary = "Retrieve the ASN info.", tags = {"Generic APIs"})
+  @Operation(summary = "Retrieve the ASN info.", tags = {"DC Generic APIs"})
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "OK")
   })
@@ -20,7 +20,7 @@ public interface DcGenericApi {
       @Parameter(required = true) String whId,
       @Parameter(required = true) String shipmentNumber);
 
-  @Operation(summary = "Create a new ASN.", tags = {"Generic APIs"})
+  @Operation(summary = "Create a new ASN.", tags = {"DC Generic APIs"})
   @ApiResponses(value = {
       @ApiResponse(responseCode = "201", description = "Created")
   })
@@ -29,7 +29,7 @@ public interface DcGenericApi {
           required = true,
           schema = @Schema(implementation = AsnDto.class)) AsnDto asnDto);
 
-  @Operation(summary = "Update the ASN.", tags = {"Generic APIs"})
+  @Operation(summary = "Update the ASN.", tags = {"DC Generic APIs"})
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "OK")
   })
@@ -38,7 +38,7 @@ public interface DcGenericApi {
           required = true,
           schema = @Schema(implementation = AsnDto.class)) AsnDto asnDto);
 
-  @Operation(summary = "Update the ASN if existed, otherwise create a new one.", tags = {"Generic APIs"})
+  @Operation(summary = "Update the ASN if existed, otherwise create a new one.", tags = {"DC Generic APIs"})
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "OK"),
       @ApiResponse(responseCode = "201", description = "Created")
@@ -48,7 +48,7 @@ public interface DcGenericApi {
           required = true,
           schema = @Schema(implementation = AsnDto.class)) AsnDto asnDto);
 
-  @Operation(summary = "Delete the existed ASN.", tags = {"Generic APIs"})
+  @Operation(summary = "Delete the existed ASN.", tags = {"DC Generic APIs"})
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "OK")
   })
@@ -57,7 +57,7 @@ public interface DcGenericApi {
       @Parameter(required = true) String shipmentNumber,
       @Parameter(required = true) String clientCode);
 
-  @Operation(summary = "Create a new ASN after delete the old one.", tags = {"Generic APIs"})
+  @Operation(summary = "Create a new ASN after delete the old one.", tags = {"DC Generic APIs"})
   @ApiResponses(value = {
       @ApiResponse(responseCode = "201", description = "Created")
   })
