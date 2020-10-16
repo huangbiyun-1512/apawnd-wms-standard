bb4895ae676c4958084e7d0b4fb723fcacb3da17
Nick <huangbiyun1512@163.com>
2020-10-16 16:10:07 +0800

    feature: ASN API

--- apawnd-wms-standard-provider/src/main/java/com/maersk/apawnd/wms/standard/component/constant/CacheConstant.java
+++ apawnd-wms-standard-provider/src/main/java/com/maersk/apawnd/wms/standard/component/constant/CacheConstant.java
@@ -5,6 +5,6 @@ public class CacheConstant {
   private CacheConstant() {
   }
 
-  public static final String CACHE_KEY_PREFIX_WMS_STANDARD = "wms.standard";
+  public static final String CACHE_KEY_PREFIX_WMS_STANDARD = "apawnd.wms.standard";
   public static final String CACHE_CONFIGURATION_KEY_DC_2H = "dc.2h";
 }
